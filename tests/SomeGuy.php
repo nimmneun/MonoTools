235f15d65fadb821c0d2ec728220e78ac448f2b2
Stefan <nimmneun@users.noreply.github.com>
2015-07-27 01:06:09 +0200

    Create SomeGuy.php

---
+++ tests/SomeGuy.php
@@ -0,0 +1,19 @@
+/**
+ * @desc dummy class SomeGuy
+ */
+class SomeGuy
+{
+    private $firstName = 'John';
+    private $lastName = 'Doe';
+    private $age = 35;
+
+    public function getFirstName() { return $this->firstName; }
+    public function getLastName() { return $this->lastName; }
+    public function getAge() { return $this->age; }
+
+    public function setFirstName($firstName) { $this->firstName = $firstName; return $this; }
+    public function setLastName($lastName) { $this->lastName = $lastName; return $this; }
+    public function setAge($age) { $this->age = $age; return $this; }
+
+    public function toArray() { return get_object_vars($this); }
+}
