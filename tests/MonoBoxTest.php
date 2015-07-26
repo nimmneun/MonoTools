7d123fb6e84003bfbe19b5f9126beea9f409042e
Stefan <nimmneun@users.noreply.github.com>
2015-07-27 00:42:09 +0200

    Update MonoBoxTest.php

--- tests/MonoBoxTest.php
+++ tests/MonoBoxTest.php
@@ -6,11 +6,11 @@ class MonoBoxTest extends \PHPUnit_Framework_TestCase
 {
     public function testDecamelize()
     {
-        $this->assertEquals('stuffs_about_to_happen', \MonoTools\MonoBox::decamelize(StuffsAboutToHappen));
+        $this->assertEquals('stuffs_about_to_happen', \MonoTools\MonoBox::decamelize('StuffsAboutToHappen'));
     }
 
     public function testCamelize()
     {
-        $this->assertEquals('StuffsAboutToHappen', \MonoTools\MonoBox::camelize(stuffs_about_to_happen));
+        $this->assertEquals('StuffsAboutToHappen', \MonoTools\MonoBox::camelize('stuffs_about_to_happen'));
     }
 }
