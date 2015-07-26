53cb7ec6cc75419708b92f18aec8d9efb7225172
Stefan <nimmneun@users.noreply.github.com>
2015-07-27 00:39:42 +0200

    Create MonoBoxTest.php

---
+++ tests/MonoBoxTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MonoTools;
+
+class MonoBoxTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDecamelize()
+    {
+        $this->assertEquals('stuffs_about_to_happen', \MonoTools\MonoBox::decamelize(StuffsAboutToHappen));
+    }
+
+    public function testCamelize()
+    {
+        $this->assertEquals('StuffsAboutToHappen', \MonoTools\MonoBox::camelize(stuffs_about_to_happen));
+    }
+}
